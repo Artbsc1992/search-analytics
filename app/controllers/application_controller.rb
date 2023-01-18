b7caf015aed2023637e794131e9d507e1b40868b
arturo <ahpareja@gmail.com>
2023-01-18 01:54:46 -0500

    Fix sign up bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :update_allowed_parameters, if: :devise_controller?
 
-
   protected
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:username, :email, :password, :password_confirmation)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
   end
 
   def after_sign_in_path_for(resource)
